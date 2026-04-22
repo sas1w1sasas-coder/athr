b9e816afe447069bfce3993d243514bd0d437e25
Carpet Land <szater600@gmail.com>
2026-04-22 12:22:46 +0200

    Switch app routing to HashRouter for Vercel admin paths

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 ﻿import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { StoreProvider } from "./store/StoreProvider";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <StoreProvider>
         <App />
       </StoreProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
